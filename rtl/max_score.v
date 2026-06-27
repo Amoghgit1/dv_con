1617bda38668a0ab9549928d5958f49be100283b
Amoghgit1 <amiable.amogh@gmail.com>
2026-06-27 11:37:23 +0530

    Modify max_score module to include clear input
    Added clear input to reset best_score and best_object.

--- rtl/max_score.v
+++ rtl/max_score.v
@@ -2,6 +2,7 @@
 module max_score(
     input clk,
     input reset,
+    input clear,
     input i_data_valid,
     input signed [31:0] final_score,
     input [15:0]object_id,
@@ -11,7 +12,7 @@ module max_score(
     output reg [15:0] best_object
     );
     always@(posedge clk)begin
-        if(!reset)begin
+        if(reset||clear)begin
             best_score   <= -32'sd2147483648;
             best_object<=0;
             o_data_valid <= 0;
@@ -26,5 +27,10 @@ module max_score(
          o_data_valid <= i_data_valid;
          end
         end      
-             
+         always @(posedge clk)
+begin
+    if(i_data_valid)
+        $display("OBJ=%0d SCORE=%0d BEST=%0d",
+                 object_id, final_score, best_object);
+end   
 endmodule
